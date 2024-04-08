e3c0a010fd8de70f8feda00ec0c677036f595e75
Aran <aranmcc7@outlook.com>
2024-04-08 18:37:25 +0100

    fixed navbar issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
-/**
-* @type {import('next').NextConfig}
-*/
-const nextConfig = {
-    output: 'export',
-    assetPrefix: process.env.ASSET_PREFIX,
-    basePath: process.env.BASE_PATH,
-    images: {
-      loader: 'akamai',
-      path: '',
-    }
-  };
-  
-  export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
