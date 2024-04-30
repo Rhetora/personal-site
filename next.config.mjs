485118ef67d1d1f492eaaf8a4b24db297abfd476
Aran <aranmcc7@outlook.com>
2024-05-01 00:09:43 +0100

    basepath change

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const env = process.env.NODE_ENV == 'production;'
+const env = process.env.NODE_ENV
 const nextConfig = {
     reactStrictMode: true,
     output: 'export',
-    basePath: env ? '/personal-site': '',
-    assetPrefix: env ? '/personal-site/': '', 
+    basePath: '/personal-site',
+    assetPrefix: '/personal-site/', 
 };
 
 export default nextConfig;
