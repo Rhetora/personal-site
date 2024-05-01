b1e1b006d59b8acd33a29bef3321077af6bfb320
Aran <aranmcc7@outlook.com>
2024-05-01 23:54:24 +0100

    navbar for mobile

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,16 +1,21 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import ThemeSwap from "@/components/ThemeBtn";
 import { ThemeContext } from "@/context/ThemeContext";
 
-const navigation = [
+interface NavigationItem {
+  name: string;
+  href: string;
+}
+
+const navigationItems: NavigationItem[] = [
   { name: "Home", href: "./" },
   //{ name: "Skills", href: "/skills" },
   { name: "Timeline", href: "./experience" },
   //{ name: "Education", href: "/education" },
-  // { name: "Projects", href: "/projects" },
+  { name: "Projects", href: "./projects" },
   // { name: "Qualifications", href: "/qualifications" },
   // { name: "Adventures", href: "/adventures" },
   // { name: "Blog", href: "/blog" },
@@ -22,17 +27,50 @@ function classNames(...classes: any) {
 
 export default function Navbar() {
   const pathname = usePathname();
-  const isActive = (href: any) => pathname == href;
+  const isActive = (href: any) => "." + pathname == href;
   const { changeTheme } = useContext(ThemeContext);
 
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
-        <a className="btn btn-ghost text-semibold text-2xl" href="/">Aran McConnell</a>
+        <a className="btn btn-ghost text-semibold text-2xl" href="./">
+          Aran McConnell
+        </a>
       </div>
-      <div className="flex-none">
+      <div className="dropdown dropdown-end lg:hidden">
+        <div tabIndex={0} role="button" className="btn btn-ghost rounded-btn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block w-6 h-6 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </div>
+        <ul tabIndex={0}
+          className="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+          {navigationItems.map((item) => (
+            <li key={item.name}>
+              <a
+                href={item.href}
+                className={classNames(isActive(item.href) ? "active" : "")}
+              >
+                {item.name}
+              </a>
+            </li>
+          ))}
+          <ThemeSwap handleOnClick={changeTheme} />
+        </ul>
+      </div>
+      <div className="flex-none hidden lg:block">
         <ul className="menu menu-horizontal text-md text-semibold px-1">
-          {navigation.map((item) => (
+          {navigationItems.map((item) => (
             <li key={item.name}>
               <a
                 href={item.href}
